c5fe23704e19464d919e16d3938caff32020005d
Ersayin Tamabay <e.tamabay@forise.group>
2023-04-23 00:46:19 +0600

    modified project details

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,34 +1,40 @@
 <template>
-  <div class="border-end bg-white" id="sidebar-wrapper" style="z-index: 2" v-if="sidebar">
-    <div class="sidebar-heading d-flex justify-content-center">
-      <img src="/images/logo/logo-immuniweb.svg" width="150">
-    </div>
-    <div class="list-group list-group-flush d-flex">
-      <NuxtLink to="/profile" class="list-group-item" :class="{'list-group-item-active':(name === 'profile')}">
-        <img src="/images/icons/profile.png" width="24" height="24"> Profile <div class="block-body-left-item-icon-arr"></div>
-      </NuxtLink>
-      <NuxtLink to="/dashboard" class="list-group-item" :class="{'list-group-item-active':(name === 'dashboard')}">
-        <img src="/images/icons/dashboard.png" width="24" height="24"> Projects <div class="block-body-left-item-icon-arr"></div>
-      </NuxtLink>
-      <NuxtLink to="/tickets" class="list-group-item" :class="{'list-group-item-active':(name === 'tickets')}">
-          <img src="/images/icons/receipt.png" width="24" height="24"> Tickets <div class="block-body-left-item-icon-arr"></div>
-      </NuxtLink>
-      <NuxtLink to="/content" class="list-group-item" :class="{'list-group-item-active':(name === 'content')}">
-          <img src="/images/icons/copy-writing.png" width="24" height="24"> Content editor <div class="block-body-left-item-icon-arr"></div>
-      </NuxtLink>
-      <NuxtLink to="/system" class="list-group-item" :class="{'list-group-item-active':(name === 'system')}">
-          <img src="/images/icons/management.png" width="24" height="24"> System <div class="block-body-left-item-icon-arr"></div>
-      </NuxtLink>
-      <NuxtLink to="/neuron" class="list-group-item" :class="{'list-group-item-active':(name === 'neuron')}">
-          <img src="/images/icons/neuron.png" width="24" height="24"> Neuron <div class="block-body-left-item-icon-arr"></div>
-      </NuxtLink>
-      <NuxtLink to="/discovery" class="list-group-item" :class="{'list-group-item-active':(name === 'discovery')}">
-          <img src="/images/icons/discover.png" width="24" height="24"> Discovery <div class="block-body-left-item-icon-arr"></div>
-      </NuxtLink>
-      <a class="list-group-item" @click="logout">
-        <img src="/images/icons/log-out.png" width="24" height="24"> Log out <div class="block-body-left-item-icon-arr"></div>
-      </a>
-    </div>
+  <div>
+      <div class="border-end bg-white" id="sidebar-wrapper" style="z-index: 2" v-if="sidebar">
+          <div class="sidebar-heading d-flex justify-content-center">
+              <img src="/images/logo/logo-immuniweb.svg" width="150">
+          </div>
+          <div class="list-group list-group-flush d-flex">
+              <NuxtLink to="/dashboard" class="list-group-item" :class="{'list-group-item-active':(name === 'dashboard')}">
+                  <img src="/images/icons/dashboard.png" width="24" height="24"> Projects <div class="block-body-left-item-icon-arr"></div>
+              </NuxtLink>
+              <NuxtLink to="/tickets" class="list-group-item" :class="{'list-group-item-active':(name === 'tickets')}">
+                  <img src="/images/icons/receipt.png" width="24" height="24"> Tickets <div class="block-body-left-item-icon-arr"></div>
+              </NuxtLink>
+              <NuxtLink to="/content" class="list-group-item" :class="{'list-group-item-active':(name === 'content')}">
+                  <img src="/images/icons/copy-writing.png" width="24" height="24"> Content editor <div class="block-body-left-item-icon-arr"></div>
+              </NuxtLink>
+              <NuxtLink to="/system" class="list-group-item" :class="{'list-group-item-active':(name === 'system')}">
+                  <img src="/images/icons/management.png" width="24" height="24"> System <div class="block-body-left-item-icon-arr"></div>
+              </NuxtLink>
+              <NuxtLink to="/neuron" class="list-group-item" :class="{'list-group-item-active':(name === 'neuron')}">
+                  <img src="/images/icons/neuron.png" width="24" height="24"> Neuron <div class="block-body-left-item-icon-arr"></div>
+              </NuxtLink>
+              <NuxtLink to="/discovery" class="list-group-item" :class="{'list-group-item-active':(name === 'discovery')}">
+                  <img src="/images/icons/discover.png" width="24" height="24"> Discovery <div class="block-body-left-item-icon-arr"></div>
+              </NuxtLink>
+          </div>
+      </div>
+      <div class="border-end bg-white" id="sidebar-wrapper" style="z-index: 2" v-if="sidebar">
+          <div class="list-group list-group-flush d-flex">
+              <NuxtLink to="/profile" class="list-group-item" :class="{'list-group-item-active':(name === 'profile')}">
+                  <img src="/images/icons/profile.png" width="24" height="24"> Profile <div class="block-body-left-item-icon-arr"></div>
+              </NuxtLink>
+              <a class="list-group-item" @click="logout">
+                  <img src="/images/icons/log-out.png" width="24" height="24"> Log out <div class="block-body-left-item-icon-arr"></div>
+              </a>
+          </div>
+      </div>
   </div>
 </template>
 
