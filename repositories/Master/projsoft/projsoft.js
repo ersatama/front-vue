97d98fc9de5e4c3d8df750028338edc0b3bb672b
Ersayin Tamabay <e.tamabay@forise.group>
2023-04-28 14:10:35 +0600

    ready 100% soft vuln monitor

--- repositories/Master/projsoft/projsoft.js
+++ repositories/Master/projsoft/projsoft.js
@@ -1,6 +1,6 @@
 export default ($axios) => ({
     getSoftVulnByProjectId(projectId) {
-        return $axios.get('https://admin3.iw:1450//api/v1/master/projsoft/getSoftVulnByProjectId/3896348'/*+projectId*/).then(response => {
+        return $axios.get('/api/v1/master/projsoft/getSoftVulnByProjectId/'+projectId).then(response => {
             return response;
         }).catch(error => {
             return error.response;
