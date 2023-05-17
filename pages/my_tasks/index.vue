8b62c2822f412640808e258c39e368a571404000
Ersayin Tamabay <e.tamabay@forise.group>
2023-05-17 15:39:55 +0600

    ready menu

---
+++ pages/my_tasks/index.vue
@@ -0,0 +1,18 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "index",
+    layout: 'admin'
+})
+</script>
+
+<template>
+    <div class="block">
+        <div class="block-title">My tasks</div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
