5e410b6e05f1009f74bed1f0a99e192c511fb8f5
Ersayin Tamabay <e.tamabay@forise.group>
2023-05-22 20:46:48 +0600

    modified project graphics and ui/ux

--- repositories/Master/scanStat/scanStat.js
+++ repositories/Master/scanStat/scanStat.js
@@ -1,6 +1,6 @@
 export default ($axios) => ({
     getScanStat(payload) {
-        return $axios.post('https://admin3.iw:1450/api/v1/master/scanStat/getScanStat', payload).then(response => {
+        return $axios.post('/api/v1/master/scanStat/getScanStat', payload).then(response => {
             return response;
         }).catch(error => {
             return error.response;
